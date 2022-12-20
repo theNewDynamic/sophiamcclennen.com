2083b3873241fa5d3c4a83cb9d4eed11c0542749
Bud Parr <budparr@thenewdynamic.com>
2022-12-20 14:20:58 -0500

    Update Hugo script

--- get-hugo.sh
+++ get-hugo.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 DIR=${PWD}
-
-HUGO_TYPE='extended'
-# Check for positional parameter, if found we assume it's the intended version
-if [ ! $1 ]; then
-#Required to be SET. TODO: Test for this, and if version is not present load from present. Use something like https://zwbetz.com/script-to-install-latest-hugo-release-on-linux-and-mac/
-    HUGO_VERSION=$(node -p "require('./package.json').hugo.version")
-    HUGO_TYPE=$(node -p "require('./package.json').hugo.extended")
-else
-    HUGO_VERSION=$1
-fi
-
+# Required to be SET.
+# Later: Test for this, and if version is not present load from present. Use something like https://zwbetz.com/script-to-install-latest-hugo-release-on-linux-and-mac/
+HUGO_VERSION=$(node -p "require('./package.json').hugo.version")
+HUGO_TYPE=$(node -p "require('./package.json').hugo.extended")
 FILE=hugo_${HUGO_VERSION}
 
 set -e
 
-if [ -n $HUGO_VERSION ]; then
+if [ $HUGO_VERSION != 'undefined' ]; then
     echo Installing $HUGO_VERSION
 else
+    echo Missing Hugo Version
     exit
 fi
 
@@ -46,7 +40,7 @@ pushd ${PWD}/temp
 
 echo Current Directory: ${PWD}
 
-curl -L https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${EXTENDED}${HUGO_VERSION}_macOS-64bit.tar.gz -o ${FILE}.tar.gz
+curl -L https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${EXTENDED}${HUGO_VERSION}_darwin-universal.tar.gz -o ${FILE}.tar.gz
 
 TARBALL=${FILE}.tar.gz
 

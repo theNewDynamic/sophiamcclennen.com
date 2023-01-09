90384cac1ae8da985cb537ce30a5be74c129fae0
Regis Philibert <login@regisphilibert.com>
2023-01-09 16:54:42 -0500

    Add site settings

--- cms/schemas/index.ts
+++ cms/schemas/index.ts
@@ -2,12 +2,14 @@ import news from './news'
 import book from './book'
 import review from './review'
 import slug from './fields/slug'
+import siteSettings from './siteSettings'
 import blockContent from './blockContent'
 
 export const schemaTypes = [
   news,
   book,
   review,
+  siteSettings,
   //Fields
   slug,
   blockContent

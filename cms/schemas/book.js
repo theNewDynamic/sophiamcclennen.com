350c55e0f6ba40563c62f5bac0f5ddde22c6ff5e
Regis Philibert <login@regisphilibert.com>
2023-01-10 10:05:18 -0500

    Fix ISBN

--- cms/schemas/book.js
+++ cms/schemas/book.js
@@ -21,27 +21,47 @@ export default defineType({
       title: 'Date',
       type: 'datetime',
     },
+{
+  name: 'editions',
+  title: 'Editions',
+  type: 'array',
+  of: [
     {
-      name: 'editions',
-      title: 'Editions',
-      type: 'array',
-      of: [
+      type: "object",
+      name: "inline",
+      fields: [
         {
-          type: "object",
-          name: "inline",
-          fields: [
-            {
-              name: 'date',
-              title: 'Date',
-              type: 'datetime',
-            },
-            { type: "string", name: "publisher" },
-            { type: "number", name: "isbn"},
-            { type: "image", name: "cover_image"},
-          ]
-        }
+          name: 'binding',
+          title: 'Binding',
+          type: 'string',
+          options: {
+            list: [
+              { title: "Hardcover", value: "hardcover" },
+              { title: "Paperback", value: "paperback" },
+              { title: "Audio", value: "audio" },
+              { title: "EBook", value: "ebook" },
+            ]
+          }
+        },
+        { name: 'title', title: 'Title', type: 'string' },
+        { name: 'date', title: 'Date', type: 'datetime'},
+        { type: "string", name: "publisher" },
+        { type: "string", name: "isbn"},
+        { type: "image", name: "cover_image"},
       ]
-    },
+    }
+  ],
+  // Still is not working but leaving it here until resolved...
+  options: {
+    preview: {
+      select: {
+        title: 'binding',
+        image: 'image.asset'
+      }
+    }
+  }
+
+},
     {
       name: 'description',
       title: 'Description',
